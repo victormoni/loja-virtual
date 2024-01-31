ad29bc604a1bb06818079cc375a6a3be714e0433
victormoni <victor-moni@hotmail.com>
2024-01-31 16:07:09 -0300

    adding json ignore

--- backend/src/main/java/com/dev/backend/entity/PermissaoPessoa.java
+++ backend/src/main/java/com/dev/backend/entity/PermissaoPessoa.java
@@ -2,6 +2,8 @@ package com.dev.backend.entity;
 
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -22,6 +24,7 @@ public class PermissaoPessoa {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @JsonIgnore
     @ManyToOne
     @JoinColumn(name = "idPessoa")
     private Pessoa pessoa;
